571ed510c4e078be69c3c1aeaf9f88c130373dc1
autumn <109412646+autumn-2-net@users.noreply.github.com>
2023-11-22 13:16:46 +0800

    add obj loader

---
+++ model_trainer/basic_lib/obj_loader.py
@@ -0,0 +1,21 @@
+def build_object_from_class_name(cls_str, parent_cls, strict, *args, **kwargs):
+    import importlib
+
+    pkg = ".".join(cls_str.split(".")[:-1])
+    cls_name = cls_str.split(".")[-1]
+    cls_type = getattr(importlib.import_module(pkg), cls_name)
+    if parent_cls is not None:
+        assert issubclass(cls_type, parent_cls), f'| {cls_type} is not subclass of {parent_cls}.'
+    if strict:
+        return cls_type(*args, **kwargs)
+    return cls_type(*args, **filter_kwargs(kwargs, cls_type))
+
+
+def filter_kwargs(dict_to_filter, kwarg_obj):
+    import inspect
+
+    sig = inspect.signature(kwarg_obj)
+    filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD]
+    filtered_dict = {filter_key: dict_to_filter[filter_key] for filter_key in filter_keys if
+                     filter_key in dict_to_filter}
+    return filtered_dict
